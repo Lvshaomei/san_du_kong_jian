6a87b9f09f585965e6e64b47c04cf658bb3fd6d0
tangmenjue <55628509+liuyingqiao2019@users.noreply.github.com>
2020-08-31 22:23:50 +0800

    Add files via upload
    注册登录等页面的基本配置文件，为了与已有的配置文件区别，将在文件名后加上_1的字符

---
+++ main_1.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App'
+import store from './jump'
+Vue.prototype.$store = store
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+new Vue({
+  store,
+ render: h=> h(App)
+}).$mount("#app")
+const app = new Vue({
+	store,
+    ...App
+})
+app.$mount()
